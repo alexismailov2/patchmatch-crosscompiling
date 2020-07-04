e1b57be40c017fb5a2c6800fbd7dfecfd5de7d73
Alexander Ismailov <alexanderismailov@MacBook-Pro-Alexander.local>
2020-07-04 17:44:19 +0300

    Update submodules.
    Signed-off-by: Alexander Ismailov <alexanderismailov@MacBook-Pro-Alexander.local>

--- build_ios_cmake.sh
+++ build_ios_cmake.sh
@@ -16,6 +16,16 @@ build_all_platforms() {
     #build_selected_platform "OS64COMBINED" $1 $2
 }
 
+if [ -d "3rdparty/opencv2.framework" ]; then
+  echo "Used prebuilt libraries"
+else
+  echo "opencv2.framework folder does not exist, starting download and unpack it first"
+  mkdir -p 3rdparty
+  cd 3rdparty
+  wget -q -O tmp.zip https://sourceforge.net/projects/opencvlibrary/files/4.3.0/opencv-4.3.0-ios-framework.zip/download && unzip tmp.zip && rm tmp.zip
+  cd ..
+fi
+
 mkdir -p ios/include
 ln -s ../../3rdparty/opencv2.framework/Headers ios/include/opencv2
 
